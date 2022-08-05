6ee693b255444960ae1de7c1a776b6a6588366b7
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2022-08-05 07:06:52 -0700

    Add all background tiles, pallets, and clean up sprites

--- src/helloworld.asm
+++ src/helloworld.asm
@@ -7,22 +7,19 @@
 .endproc
 
 .proc nmi_handler
-    ; ; Prepare to transfer to OAM at byte 0
-    ; LDA #$00
-    ; STA OAMADDR
-
-    ; ; transfer the 256 bytes of data at $0200 - $02FF
-    ; LDA #$02
-    ; STA OAMDMA
-
-    ; RTI
+    ; Prepare to transfer to OAM at byte 0
     LDA #$00
     STA OAMADDR
+    ; transfer the 256 bytes of data at $0200 - $02FF
     LDA #$02
     STA OAMDMA
+
+    ; set scrolling window to 0,0
+    ; https://www.nesdev.org/wiki/PPU_scrolling
     LDA #$00
-    STA $2005
-    STA $2005
+    STA PPUSCROLL
+    STA PPUSCROLL
+
     RTI
 .endproc
 
@@ -43,16 +40,16 @@ load_palettes:
     LDA palettes, X         ; load the pallet color with offest
     STA PPUDATA             ; store it in the PPU and auto increment store location
     INX                     ; increment the X reg
-    CPX #$10                ; set flags
+    CPX #$20                ; set flags
     BNE load_palettes       ; branch if Zero flag unset, we are not at 4 yet
 
     LDX #$00
 load_sprites:
     ; Now write some sprite data
     LDA sprites,X
-    STA $0200,X             ; Here we write to an indexed memory location which is equally offset
+    STA SPRITE_PAGE,X             ; Here we write to an indexed memory location which is equally offset
     INX
-    CPX #$20
+    CPX #$10                      ; only use one sprite group (4 tiles) (change to $20 for both)
     BNE load_sprites
 
 load_nametable:
@@ -103,8 +100,23 @@ load_small_star1:
     LDY small_star1         ; load the tile into Y
     STY PPUDATA             ; write the tile
 
-    INX                     ; move to the next bit pair
-    CPX #$15                ; we have 8 bits, but only 4 bit pairs, but we start at 1
+    ; --- attribute data
+    LDA PPUSTATUS
+
+    INX
+    LDA large_star,X        ; read the low bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA large_star,X        ; read the high bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA large_star,X        ; read the attribute flags
+    STA PPUDATA             ; and write them
+
+    INX                     ; move to the next tile set
+    CPX #$15                ; we have 8 bits, but only 4 tile sets, but we start at 1
     BNE load_small_star1    ; if we haven't read them all loop
 ; ----------- Small Star 2 -------------------------
     LDX #$01                ; we'll set the offset to the position after the tile
@@ -120,9 +132,56 @@ load_small_star2:
     LDY small_star2         ; load the tile into Y
     STY PPUDATA             ; write the tile
 
-    INX                     ; move to the next bit pair
-    CPX #$15                ; we have 8 bits, but only 4 bit pairs, but we start at 2
+    ; --- attribute data
+    LDA PPUSTATUS
+
+    INX
+    LDA large_star,X        ; read the low bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA large_star,X        ; read the high bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA large_star,X        ; read the attribute flags
+    STA PPUDATA             ; and write them
+
+    INX                     ; move to the next tile set
+    CPX #$15                ; we have 8 bits, but only 4 tile sets, but we start at 1
     BNE load_small_star2    ; if we haven't read them all loop
+; ----------- Satellite -------------------------
+    LDX #$01                ; we'll set the offset to the position after the tile
+load_satellite:
+    LDA PPUSTATUS           ; clear PPUADDR by reading PPUSTATUS
+
+    LDA satellite,X         ; read the low bit
+    STA PPUADDR             ; and store it
+    INX                     ; move the index to the high bit
+    LDA satellite,X         ; read the high bit
+    STA PPUADDR             ; store it
+
+    LDY satellite           ; load the tile into Y
+    STY PPUDATA             ; write the tile
+
+    ; --- attribute data
+    LDA PPUSTATUS
+
+    INX
+    LDA satellite,X         ; read the low bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA satellite,X         ; read the high bit
+    STA PPUADDR             ; and write it
+
+    INX
+    LDA satellite,X         ; read the attribute flags
+    STA PPUDATA             ; and write them
+
+    INX                     ; move to the next tile set
+    CPX #$06                ; we have 8 bits, but only 1 tile sets, but we start at 1
+    BNE load_satellite      ; if we haven't read them all loop
 ; ------------------------------------
 
 
@@ -151,16 +210,13 @@ forever:
 .segment "RODATA"       ; Read Only Data
 ; ----------------- PALETTES ------------------------
 palettes:
-; .byte $0d, $0c, $1c, $2c
-; .byte $0d, $19, $29, $3a
-; .byte $0d, $00, $10, $20
-; .byte $0d, $06, $16, $26
-
+background_palettes:
 .byte $0f, $12, $23, $27
 .byte $0f, $2b, $3c, $39
 .byte $0f, $0c, $07, $13
 .byte $0f, $19, $09, $29
 
+sprite_palettes:
 .byte $0f, $2d, $10, $15
 .byte $0f, $19, $09, $29
 .byte $0f, $19, $09, $29
@@ -189,25 +245,31 @@ large_star:
 ;     T#    
 .byte $2f
 ;     thi  tlo  ahi  alo  atable
-.byte $20, $6b, $23, $ca, %01000000
-.byte $21, $57, $23, $ca, %01000000
-.byte $22, $23, $23, $ca, %01000000
-.byte $23, $52, $23, $ca, %01000000
+.byte $20, $6b, $23, $c2, %01000000
+.byte $21, $57, $23, $d5, %01000000
+.byte $22, $23, $23, $e0, %00000100
+.byte $23, $52, $23, $f4, %01000000
 
 small_star1:
 ;     T#    
 .byte $2d
 ;      hi lo    ahi  alo  atable 
-.byte $20, $c3, $23, $ca, %01000000
-.byte $21, $1d, $23, $ca, %01000000
-.byte $21, $93, $23, $ca, %01000000
-.byte $23, $28, $23, $ca, %01000000
+.byte $20, $c3, $23, $c8, %01000000
+.byte $21, $1d, $23, $d7, %00000001
+.byte $21, $93, $23, $dc, %00000100
+.byte $23, $28, $23, $f2, %00000001
 
 small_star2:
 ;     T#    
 .byte $2e
 ;      hi lo    ahi  alo  atable 
-.byte $20, $77, $23, $ca, %01000000
-.byte $21, $3a, $23, $ca, %01000000
-.byte $22, $4b, $23, $ca, %01000000
-.byte $23, $3f, $23, $ca, %01000000
+.byte $20, $77, $23, $c5, %01000000
+.byte $21, $3a, $23, $d6, %00000100
+.byte $22, $4b, $23, $e2, %01000000
+.byte $23, $3f, $23, $f7, %00000100
+
+satellite:
+;     T#    
+.byte $65
+;      hi lo    ahi  alo  atable 
+.byte $20, $5d, $23, $c7, %00100000
--- src/reset.asm
+++ src/reset.asm
@@ -11,6 +11,17 @@
     LDX #$00
     STX PPUCTRL
     STX PPUMASK
+
+; hide all sprites off screen to prevent artifacting
+    LDX #$00
+    LDA #$ff
+clear_oam:
+    STA SPRITE_PAGE,X
+    INX ; there are 4 bytes in sprite data
+    INX
+    INX
+    INX
+    BNE clear_oam
 vblankwait:
     BIT PPUSTATUS
     BPL vblankwait
