97cbf1f0a7b2c98c40d03517b66a5b2bc9556a25
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2022-08-20 07:35:53 -0700

    Updates

---
+++ build/precache-manifest.576ce95e0ef42974599a1a89c6281cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd9a23b5be600d377d96a6511fb373a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b380d0f607d59d0273c",
+    "url": "./static/css/main.4f956c73.chunk.css"
+  },
+  {
+    "revision": "e01fade06c6ed4afa679",
+    "url": "./static/js/2.b9752613.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.b9752613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b380d0f607d59d0273c",
+    "url": "./static/js/main.10c59292.chunk.js"
+  },
+  {
+    "revision": "58b46143ead25e416ea9",
+    "url": "./static/js/runtime-main.ecb09f17.js"
+  }
+]);
