e84064a516c05af179177c37b568fbe1fe1fa3b0
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2022-08-20 07:39:18 -0700

    Updates

---
+++ build/precache-manifest.4c939ae9a7575abfd9e780f9e9ce450a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e907ec9f128431fe10788282cf14ae",
+    "url": "/FamicomPartyHelloWorld/index.html"
+  },
+  {
+    "revision": "716f83436f6c431ced70",
+    "url": "/FamicomPartyHelloWorld/static/css/main.4f956c73.chunk.css"
+  },
+  {
+    "revision": "e01fade06c6ed4afa679",
+    "url": "/FamicomPartyHelloWorld/static/js/2.b9752613.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/FamicomPartyHelloWorld/static/js/2.b9752613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716f83436f6c431ced70",
+    "url": "/FamicomPartyHelloWorld/static/js/main.52907883.chunk.js"
+  },
+  {
+    "revision": "80058b26fdcc93ff34c4",
+    "url": "/FamicomPartyHelloWorld/static/js/runtime-main.e2cfcc91.js"
+  }
+]);
