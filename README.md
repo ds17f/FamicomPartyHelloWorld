f17d2b517190af2b02174ff99ae675cb917e39ce
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2022-08-21 15:01:10 -0700

    Add the web emulator and notes about it

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Famicom Party Hello World 
 Following the tutorial here: https://famicom.party
 
+## Playable Demo
+You can play this game by going here:
+[https://ds17f.github.io/FamicomPartyHelloWorld](https://ds17f.github.io/FamicomPartyHelloWorld)
+
+Huge shoutout to [jsnes](https://github.com/bfirsh/jsnes) and [jsnes-web](https://github.com/bfirsh/jsnes-we://github.com/bfirsh/jsnes-web) for building an emulator that runs in JS and on the Web
+
+
 ## Prerequisits
 * FCEUX installed
 * Python 3 installed
---
+++ jsnes-web/precache-manifest.615ee0b26efafa6fb918b2d176e15778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73031f414ebdfe76aa2cddbe14b1c67e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5e7018c28a755eb00a4",
+    "url": "./static/css/main.a881b934.chunk.css"
+  },
+  {
+    "revision": "171ddec32e168ebb4d8c",
+    "url": "./static/js/2.ea036b3c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.ea036b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e7018c28a755eb00a4",
+    "url": "./static/js/main.39fa52a9.chunk.js"
+  },
+  {
+    "revision": "58b46143ead25e416ea9",
+    "url": "./static/js/runtime-main.ecb09f17.js"
+  }
+]);
